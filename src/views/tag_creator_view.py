956d26abf869da03d05d67cae160c151523e0dfd
Rafael Freitas <rafael.afmendonca1994@gmail.com>
2024-02-07 10:57:36 -0300

    feat: implementing the barcode to project

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,3 +1,4 @@
+from src.controllers.tag_creator_controller import TagCreatorController
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 
@@ -5,13 +6,14 @@ from src.views.http_types.http_response import HttpResponse
 class TagCreatorView:
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        body = http_request.body
+        tag_creator_controller = TagCreatorController()
 
+        body = http_request.body
         product_code = body["product_code"]
+
+        formatted_response = tag_creator_controller.create(product_code)
 
         return HttpResponse(
             status_code=200,
-            body={
-                "product_code": product_code,
-            },
+            body=formatted_response,
         )
