8af13cc6d4234fa8fa72e1c3a9abf31f1619bdd5
Rafael Freitas <rafael.afmendonca1994@gmail.com>
2024-02-08 15:05:21 -0300

    test: implementing unit test with pytest

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,7 +1,21 @@
 from src.views.http_types.http_response import HttpResponse
 
+from .error_types.http_unprocessable_entity import HttpUnprocessabelEntityError
+
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessabelEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [
+                    {
+                        "title": "Unprocessable Entity",
+                        "description": error.message,
+                    }
+                ],
+            },
+        )
     return HttpResponse(
         status_code=500,
         body={
